bd15c8a38dd5fd54f4261f1c6b2680cbe1e797ea
dengjinyao <jinyaodeng@126.com>
2022-07-11 18:47:42 +0800

    配置redis

--- src/main/java/com/deng/miaosha/config/RedisConfig.java
+++ src/main/java/com/deng/miaosha/config/RedisConfig.java
@@ -1,11 +1,53 @@
 package com.deng.miaosha.config;
 
+import com.deng.miaosha.serializer.JodaDateTimeJsonDeserializer;
+import com.deng.miaosha.serializer.JodaDateTimeJsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.joda.time.DateTime;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.stereotype.Component;
+
+import java.net.UnknownHostException;
 
 
-@Component
-//将 Session存到 Redis中,并设置过期时间为 3600s
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+@Configuration
+////将 Session存到 Redis中,并设置过期时间为 3600s
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        //key的序列化方式(用String的序列化方式)
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+
+        //value的序列化方式(用JSON的序列化方式)
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        //指定JodaDateTime的序列化实现
+        ObjectMapper objectMapper =  new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(DateTime.class,new JodaDateTimeJsonSerializer());
+        simpleModule.addDeserializer(DateTime.class,new JodaDateTimeJsonDeserializer());
+
+        //添加类型信息
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        objectMapper.registerModule(simpleModule);
+
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+        return template;
+    }
+
 }
