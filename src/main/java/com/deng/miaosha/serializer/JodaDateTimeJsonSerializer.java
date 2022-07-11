3b12c6417b0e518f3d6b750749e5c8abae811531
dengjinyao <jinyaodeng@126.com>
2022-07-11 18:48:25 +0800

    实现对JodaDataTime的json序列化

---
+++ src/main/java/com/deng/miaosha/serializer/JodaDateTimeJsonSerializer.java
@@ -0,0 +1,17 @@
+package com.deng.miaosha.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+
+//定义JodaDateTime的JSON序列化实现
+public class JodaDateTimeJsonSerializer extends JsonSerializer<DateTime> {
+    @Override
+    public void serialize(DateTime dateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(dateTime.toString("yyyy-MM-dd HH:mm:ss"));
+    }
+}
