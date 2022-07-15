152f3e633c1bb2fbdb85818c8e33583bb2d7d5ff
dengjinyao <jinyaodeng@126.com>
2022-07-15 21:13:01 +0800

    多环境配置

--- src/main/java/com/deng/miaosha/config/CorsConfig.java
+++ src/main/java/com/deng/miaosha/config/CorsConfig.java
@@ -2,10 +2,17 @@ package com.deng.miaosha.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+/*
+ 由于本地开发时前后端分离且不在同一端口,会使用ajax发起跨域请求,需要使用该配置
+ 但在生产环境中会使用nginx作反向代理，前后端在同一端口，不需要使用该配置
+*/
+@Profile("dev")
+//配置跨域相关的属性
 @Configuration
 public class CorsConfig {
     @Bean
@@ -20,9 +27,10 @@ public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.addAllowedOrigin("http://localhost:10000"); // 允许域名
-        corsConfiguration.addAllowedOrigin("http://127.0.0.1:10000");
+        //当AllowCredentials=true时,AllowedOrigin不能为 *
+        corsConfiguration.addAllowedOrigin("http://127.0.0.1:8088"); // 允许域名
 
+        //需要和前端的xhrFields:{withCredentials:true}配合使用
         corsConfiguration.setAllowCredentials(true); // 允许cookie
 
         corsConfiguration.addAllowedHeader("*"); // 允许头
